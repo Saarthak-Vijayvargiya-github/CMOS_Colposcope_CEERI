5ac9d9df311cf743fbe499b5feb155595a324623
Saarthak-Vijayvargiya-github <saarthak.vijayvargiya@gmail.com>
2023-07-16 17:27:30 +0530

    First Commit

--- main1.py
+++ main1.py
@@ -61,7 +61,7 @@ def initSerial():
 
     serial_inst.baudrate = 9600
     serial_inst.port = port_var
-    serial_inst.timeout = 0.05           # If this is increased, then speed of capturing will decrease
+    serial_inst.timeout = 0.03           # If this is increased, then speed of capturing will decrease
     serial_inst.open()
 
 def serialCommFocus():
